20d190e7ee24d7de35a27abcebeca53e52e72185
Rob Bygrave <robin.bygrave@gmail.com>
2025-07-07 21:29:32 +1200

    Fix pom names after rename

--- avaje-simple-logger/src/main/java/module-info.java
+++ avaje-simple-logger/src/main/java/module-info.java
@@ -4,8 +4,10 @@ module io.avaje.simplelogger.dynamic {
 
   exports io.avaje.simplelogger.dynamic;
 
+  requires transitive org.slf4j;
   requires transitive io.avaje.simplelogger;
   requires transitive io.avaje.config;
+  requires io.avaje.applog;
 
   provides io.avaje.config.ConfigExtension with DynamicLogLevels;
 }
